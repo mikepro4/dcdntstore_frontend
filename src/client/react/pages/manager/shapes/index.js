44a67aaf36411c497d40ea9a226b8f49dd4ee7b0
Mikhail Proniushkin <mikhailpro4@gmail.com>
2019-01-16 22:29:28 -0500

    buttons

--- src/client/react/pages/manager/shapes/index.js
+++ src/client/react/pages/manager/shapes/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
+import { Icon, Button } from "@blueprintjs/core";
 
 class ShapesPage extends Component {
 	state = {
@@ -16,7 +17,57 @@ class ShapesPage extends Component {
 	render() {
 		return (
             <div className="route-container route-shapes">
-                shapes
+                <div className="route-header">
+					<div className="route-header-left">
+						<div className="route-title">Shapes</div>
+					</div>
+
+					<div className="route-header-right">
+						<ul className="route-actions">
+							<li>
+								<Button
+									icon="add"
+									intent="primary"
+									text="Add new shape"
+								/>
+							</li>
+							<li>
+								<Button
+									icon="add"
+									text="Add new shape"
+								/>
+							</li>
+
+							<li>
+								<Button
+									icon="add"
+									intent="warning"
+									text="Add new shape"
+								/>
+							</li>
+
+							<li>
+								<Button
+									icon="add"
+									intent="success"
+									text="Add new shape"
+								/>
+							</li>
+
+							<li>
+								<Button
+									icon="add"
+									intent="danger"
+									text="Add new shape"
+								/>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div className="route-content">
+					content
+				</div>
             </div>
 		);
 	}
